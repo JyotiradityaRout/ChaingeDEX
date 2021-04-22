65a265df3517ab83fd00d0127da876d85a8bd682
BuildTools <roderick_lu@163.com>
2021-04-22 14:38:18 +0800

    test

--- scripts/deployRouter2.js
+++ scripts/deployRouter2.js
@@ -1,62 +1,6 @@
 const hre = require("hardhat");
 const utils = require('./utils/utils');
 
-// async function main() {
-//     const [forLiquidity, forSwap] = await hre.ethers.getSigners();
-//     console.log('\x1B[32m%s\x1B[40m', '-------------- Start --------------')
-//     const amountA = utils.addZero(1, 16);
-//     const amountB = utils.addZero(35, 16);
-//     let { tokenA, tokenB } = await utils.mint(
-//         forLiquidity,
-//         forSwap,
-//         utils,
-//         {
-//             startTime: parseInt(Date.now() / 1000),
-//             endTime: 666666666666,
-//             amountA,
-//             amountB,
-//             // 流动性充的量
-//             amountADesired: utils.addZero(1, 15),
-//             amountBDesired: utils.addZero(4, 16),
-//             // 下面两两比例相同
-//             amountAMin: utils.addZero(9, 10),
-//             amountBMin: utils.addZero(4, 10),
-//             amountOut: utils.addZero(9, 13),
-//             amountInMax: utils.addZero(4, 13)
-//         }
-//     )
-//     // x*y=k
-//     //  
-//     const balanceA = await utils._checkBalance([0, 666666666666], forSwap, tokenA)
-//     const balanceB = await utils._checkBalance([0, 666666666666], forSwap, tokenB)
-//     const balanceC = await utils._checkBalance([0, 666666666666], forLiquidity, tokenA)
-//     const balanceD = await utils._checkBalance([0, 666666666666], forLiquidity, tokenB)
-
-//     const swap_k = (amountA - balanceC) * (amountB - balanceD)
-
-//     console.log('---------- mint后 ----------')
-//     console.log('TokenA: ', amountA)
-//     console.log('TokenB: ', amountB)
-//     console.log('---------- swap后 ----------')
-//     console.log('TokenA: ', balanceA)
-//     console.log('TokenB: ', balanceB)
-//     console.log('---------- addLiquidity后 ----------')
-//     console.log('TokenA: ', balanceC)
-//     console.log('TokenB: ', balanceD)
-
-//     console.log('k: 充了流动性的A * 充了流动性的B', swap_k)
-
-//     const tempA = swap_k / (balanceD - balanceB) + balanceC
-//     console.log('手动计算swap后的tokenA内应该有', tempA)
-//     console.log('差值', balanceA - tempA)
-
-//     console.log('-------------- swap --------------')
-//     console.log('\x1B[32m%s\x1B[0m', '-------------- End --------------')
-//     return { balanceA, balanceB }
-// }
-
-
-
 async function main() {
     const [forLiquidity, forSwap] = await hre.ethers.getSigners();
     const amountA = utils.addZero(1, 18);
@@ -75,8 +19,8 @@ async function main() {
         amountAMin: utils.addZero(1, 12),
         amountBMin: utils.addZero(4, 12),
         // swap
-        amountOut: utils.addZero(1, 12),
-        amountInMax: utils.addZero(4, 12),
+        amountOut: utils.addZero(1, 10),
+        amountInMax: utils.addZero(4, 10),
         // removeLiquidity
         // liquidity: utils.addZero(1, 12),
         amountAMin: 0,
@@ -119,11 +63,11 @@ async function main() {
     console.log(`k: ${removeDeltaA * removeDeltaB * Math.pow(deltaA / removeDeltaA, 2)}`)
 
 
-    // await sleep()
-    // await utils.swap(forSwap, uniRouter, tokenA.address, tokenB.address, config)
-    // const afterSwap = await utils.checkBalance(forSwap, tokenA, tokenB, config)
-    // console.log(`swap后，tokenA的理论值：${k / (deltaB)}`)
-    // console.log(`result：${k / (deltaB) === deltaA}`)
+    await sleep()
+    await utils.swap(forSwap, uniRouter, tokenA.address, tokenB.address, config)
+    const afterSwap = await utils.checkBalance(forSwap, tokenA, tokenB, config)
+    console.log(`swap后，tokenA的理论值：${k / (deltaB)}`)
+    console.log(`result：${k / (deltaB) === deltaA}`)
 
 }
 
