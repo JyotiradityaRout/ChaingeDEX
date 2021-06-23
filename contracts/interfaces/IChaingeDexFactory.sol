a010bb1459f107f7aa05b8ad9bfab9285501033d
zhoushuren <zhoujun247@foxmail.com>
2021-06-23 17:42:59 +0800

    commit createPair sort

--- contracts/interfaces/IChaingeDexFactory.sol
+++ contracts/interfaces/IChaingeDexFactory.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.16;
 
 interface IChaingeDexFactory {
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint, uint256[] time);
 
     function feeTo() external view returns (address);
     function feeToSetter() external view returns (address);
